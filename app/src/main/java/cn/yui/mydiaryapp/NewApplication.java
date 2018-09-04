7ae26267ed8592609dac2040b73fe55d90633b79
liaoyuhuan@hudunsoft.com <liao5744369>
2018-09-04 15:31:25 +0800

    添加application

---
+++ app/src/main/java/cn/yui/mydiaryapp/NewApplication.java
@@ -0,0 +1,22 @@
+package cn.yui.mydiaryapp;
+
+import android.app.Application;
+
+
+/**
+ * @author liaoyuhuan
+ * @name NewApplication
+ * @time 2018/9/4 0004
+ */
+
+public class NewApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+}
